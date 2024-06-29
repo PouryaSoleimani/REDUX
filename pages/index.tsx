abe21159bb831b782c1562734b99527f3db6bb16
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 13:02:13 -0700

    Getting Started

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="py-2 text-4xl font-extrabold bg-purple-700 text-center border-b-8">REDUX PAGE</h1>
+      <h1 className="py-2 text-4xl font-extrabold bg-purple-700 text-center border-b-8">REDUX HOME PAGE</h1>
     </main>
   );
 }
