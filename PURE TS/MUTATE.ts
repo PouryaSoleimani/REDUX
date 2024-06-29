6cec9b4463b50a7da875a84577ed0de8fb59f8c3
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 16:23:07 -0700

    MUTATE

--- PURE TS/MUTATE.ts
+++ PURE TS/MUTATE.ts
@@ -24,4 +24,12 @@ obj2.title = "REAL TITLE" // MUTATING => MUTABLE
 
 console.log(obj1, obj2)
 
+// METHODS WITH MUTATE :( MUTATE = CHANGING THE ORIGINAL VARIBALE )
+let array = [1, 2, 3, 4, 5, 6, 7]
+let newArray = array.map(item => item * 2) // -> .map doesn't mutate the array
+console.log(array, newArray) // => array = [1, 2, 3, 4, 5, 6, 7] && newArray = [2,4,6,8,10,12,14]
 
+// METHODS WITHOUT MUTATE : ( MUTATE = CHANGING THE ORIGINAL VARIBALE )
+let array2 = [10, 20, 30, 40, 50, 60]
+let reversedArray = array2.reverse() //-> .reverse() mutates the array
+console.log(array2, reversedArray) // array2 = [60,50,40,30,20,10] && reversedArray = [60,50,40,30,20,10]
