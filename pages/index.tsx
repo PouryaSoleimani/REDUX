5a47650711cec0f07728f4493a033d17c67c2ffc
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 15:01:21 -0700

    HOF

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   function randomIdGenerator2() {
     return Math.ceil(Math.random() * 10000000).toString().slice(0, 3)
   }
+
   function userGenerator(username: string, IDgenerator: Function) {
     const userDetail = `${username} | ID: ${IDgenerator()}`
     setUser(userDetail)
@@ -19,8 +20,8 @@ export default function Home() {
   return (
     <main>
       <h1 className="py-2 text-4xl font-extrabold bg-purple-700 text-center border-b-8">REDUX HOME PAGE</h1>
-      <h2 className="text-2xl text-center py-6 font-bold bg-purple-300 text-black">{user.toString()}</h2>
-      
+      <h2 className="text-2xl text-center py-6 font-extrabold bg-purple-300 text-black">{user.toString()}</h2>
+
     </main>
   );
 }
