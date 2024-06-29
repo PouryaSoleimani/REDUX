865b7259eebf2b259d716283736a81ddc69e1580
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 16:02:34 -0700

    MUTATE

--- PURE TS/MUTATE.ts
+++ PURE TS/MUTATE.ts
@@ -1,1 +1,14 @@
 //^ MUTATE ===================================================================================================================================================================>
+// MUTATE
+// IMMUTATE
+// MUTABLE
+// IMMUTABLE
+
+// PRIMITIVE TYPES
+// REFERENCE TYPES
+
+
+
+let number1 = 12
+let number2 = number1
+number2 = 8
