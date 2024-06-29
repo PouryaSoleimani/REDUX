61b3dae1620d43fd8e63772c893a811e81fd5651
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 13:35:54 -0700

    reduce

--- PURE TS/reduce.ts
+++ PURE TS/reduce.ts
@@ -1,1 +1,10 @@
 //* REDUCE METHOD
+const myArray = [1, 2, 3, 4, 5, 6]
+
+const arrayOutput = myArray.reduce((a, b) => {
+  console.log(a, b)
+  return a + b
+})
+
+console.log(arrayOutput)
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
+import { useEffect } from "react"
+
 //^ HOME PAGE ___________________________________________________________________________________________________________________________________________________________________ 
 export default function Home() {
-  const myArray = [1, 2, 3, 4, 5, 6]
+  useEffect(() => {
+    const myArray2 = [10, 20, 30, 40, 50, 60]
+    const reducedArray = myArray2.reduce((x, y) => { console.log(x, y); return x + y })
+    console.log(reducedArray)
+  }, [])
 
-  const arrayOutput = myArray.reduce((a, b) => {
-    console.log(a, b)
-    return a + b
-  })
-
-  console.log(arrayOutput)
   return (
     <main>
       <h1 className="py-2 text-4xl font-extrabold bg-purple-700 text-center border-b-8">REDUX HOME PAGE</h1>
