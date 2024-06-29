57c5d42f4c8e7828839c43c4765e2b8d97e1447c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 16:12:03 -0700

    MUTATE

--- PURE TS/MUTATE.ts
+++ PURE TS/MUTATE.ts
@@ -8,7 +8,21 @@
 // REFERENCE TYPES
 
 
-
+// PRIMITIVE TYPES
 let number1 = 12
 let number2 = number1
-number2 = 8
+number2 = 8 // REASIGGN => IMMUTABLE
+
+console.log(number1, number2)
+
+
+// REFERENCE TYPES
+let obj1 = { id: 1, title: "FAKE TITLE" }
+let obj2 = obj1
+
+obj2.id = 50
+obj2.title = "REAL TITLE" // MUTATING => MUTABLE
+
+console.log(obj1, obj2)
+
+
