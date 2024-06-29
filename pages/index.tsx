33ad8baa7e896941a720262f036332e0fffa66a4
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 12:29:13 -0700

    Getting Started

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 //^ HOME PAGE ___________________________________________________________________________________________________________________________________________________________________ 
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+
+
+
+
+
 
 export default function Home() {
   return (
