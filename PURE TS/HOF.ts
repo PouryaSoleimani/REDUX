48bf0588c4643f443d73338571bba563abe64334
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-29 14:42:35 -0700

    HOF

--- PURE TS/HOF.ts
+++ PURE TS/HOF.ts
@@ -1,15 +1,16 @@
 //^ HIGH ORDER FUNCTIONS ===========================================================================================================================================================> 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+//* A HOF
+let evenNumbers = numbers.filter((number: number) => { // HOF functions accept functions and return functions , or accept functions and return datas 
+  return number % 2 === 0
+})
+console.log(evenNumbers)
 
 //! NOT A HOF
 function randomNumberGenerator() { return Math.ceil(Math.random() * 1000) }
 
-//* A HOF
-let evenNumbers = numbers.filter((number: number) => {
-  return number % 2 === 0
-})
-console.log(evenNumbers)
+
 
 
 //RANDOM ID GENERATOR
