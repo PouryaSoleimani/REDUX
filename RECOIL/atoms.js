48ef3ee79300ac35fa034839e3c5c5b8ed2adf3e
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 11:35:32 -0700

    TODOLIST

--- RECOIL/atoms.js
+++ RECOIL/atoms.js
@@ -1,4 +1,4 @@
-//^ ATOMS ==========================================================================================================================================================================>
+//^ ATOMS =========================================================================================================================================================================>
 import { atom } from "recoil";
 import { recoilPersist } from 'recoil-persist'
 const { persistAtom } = recoilPersist()
@@ -6,4 +6,4 @@ const { persistAtom } = recoilPersist()
 export const COUNTERSTATE = atom({ key: 'counterstate', default: 0, effects_UNSTABLE: [persistAtom] });
 
 
-export const TODOS = atom({ key: 'todos', default: [], effects_UNSTABLE: [persistAtom] });
+export const TODOS = atom({ key: 'todos', default: [], effects_UNSTABLE: [persistAtom] });s
