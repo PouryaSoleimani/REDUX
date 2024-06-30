fc5c1bfd6c2955dfaf8d6d30d0ff9384bb4db7b1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 09:03:45 -0700

    TODO LIST

--- RECOIL/atoms.js
+++ RECOIL/atoms.js
@@ -5,6 +5,4 @@ const { persistAtom } = recoilPersist()
 
 export const COUNTERSTATE = atom({ key: 'counterstate', default: 0, effects_UNSTABLE: [persistAtom] });
 
-export const TODOS = atom({
-  key: 'todos', default: [{ id: 1, title: "GO TO GYM", isCompleted: false, effects_UNSTABLE: [persistAtom], }],
-});
+export const TODOS = atom({ key: 'todos', default: [], effects_UNSTABLE: [persistAtom] });
--- pages/todolist.tsx
+++ pages/todolist.tsx
@@ -4,7 +4,8 @@ import { useRecoilState } from 'recoil'
 import { recoilPersist } from 'recoil-persist'
 const { persistAtom } = recoilPersist()
 
-//& COMPONENT
+type SingleTodoType = { id: number, title: string, isCompleted: boolean }
+//^ COMPONENT ____________________________________________________________________________________________________________________________________________________________________________
 const TodoListPage = () => {
 
   const [Todos, setTodos] = useRecoilState(TODOS)
@@ -20,18 +21,16 @@ const TodoListPage = () => {
   }
 
   return (
-    <main className='w-screen h-screen flex flex-col'>
+    <main suppressHydrationWarning className='w-screen h-screen flex flex-col'>
 
       <form id="input" onSubmit={addTodoHandler} className='w-screen flex py-2 items-center justify-center space-x-1 bg-gradient-to-r from-slate-900 via-slate-800 to-zinc-900'>
         <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder='Add Todo' className='px-2 py-1 rounded-md text-black font-bold ' />
         <button type='submit' className='bg-green-600 p-1 rounded-md'>+</button>
       </form>
 
-      <div id="todos">
+      <div id="todos" suppressHydrationWarning >
         <ul className='w-fit h-fit px-6 py-10 border list-inside list-disc mx-auto mt-10 bg-slate-900/50 flex flex-col text-justify items-center justify-center text-3xl font-bold'>
-          {Todos.map(item => (
-            <li key={item.id}>{item.title}</li>
-          ))}
+          {Todos?.map((item: SingleTodoType) => (<li key={item.id}>{item.title}</li>))}
         </ul>
       </div>
     </main>
