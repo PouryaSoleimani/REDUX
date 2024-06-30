8ffe981ffe023684a1c4b25bbe58ce1ffd81514a
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 03:59:42 -0700

    STORE

---
+++ PURE TS/REDUX_ACTION.ts
@@ -0,0 +1,18 @@
+//^ REDUX ACTION ======================================================================================================================================
+//* COUNTER ACTIONS
+let incrementAction = { type: 'INCREMENT' }
+let decrementAction = { type: 'DECREMENT' }
+
+// TODOLIST ACTIONS
+let createTodoAction = { type: "ADD_TODO" }
+let removeTodoAction = { type: "REMOVE_TODO" }
+let updateTodoAction = { type: "UPDATE_TODO" }
+let getTodosAction = { type: "GET_TODOS" }
+
+
+//? SHOP ACTION 
+let addProductAction = { type: "ADD_PRODUCT" }
+let removeProductAction = { type: "REMOVE_PRODUCT" }
+let editProductAction = { type: "EDIT_PRODUCT" }
+let addToCartAction = { type: "ADD_TO_CART" }
+let removeFromCartAction = { type: "REMOVE_FROM_CART" } 
--- PURE TS/REDUX_STORE.ts
+++ PURE TS/REDUX_STORE.ts
@@ -1,15 +1,15 @@
 //^ REDUX STORE =================================================================================================================================================>
-// COUNTER STORE 
+//* COUNTER STORE 
 let counterStore = 0 //Number Type Store Variable
 
 
-// LIST STORE 
+//TODOLIST STORE 
 export let todolistStore = [ // Array Type Store Variable
   { id: 1, title: "learn JS", isCompleted: false },
   { id: 1, title: "learn REDUX", isCompleted: false },
 ]
 
-// SHOP STORE 
+//? SHOP STORE 
 let shopStore = { // Object Type Store Variable
   prodcuts: [
     { id: 1, title: "pants", price: 32 },
