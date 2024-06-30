15c78198fafd6606dded5df61a3f0004a1ff8be1
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 07:26:53 -0700

    'counter

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -1,1 +1,17 @@
 //^ COUNTER PAGE =========================================================================================================================================================>
+import React from 'react'
+
+const CounterPage = () => {
+  return (
+    <>
+      <div>
+        <h1 className='bg-gradient-to-r from-purple-800 via-orange-600 to-yellow-400 text-3xl font-extrabold py-2 text-center'>COUNTER PAGE</h1>
+      </div>
+      <div id="Container">
+      
+      </div>
+    </>
+  )
+}
+
+export default CounterPage
