dafb962b0e46a5332fe1bff58b3c95da72ec3dfd
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 08:09:49 -0700

    todolist

--- RECOIL/atoms.js
+++ RECOIL/atoms.js
@@ -1,7 +1,10 @@
 //^ ATOMS
 import { atom } from "recoil";
 
-
 export const COUNTERSTATE = atom({ key: 'counterstate', default: 0, });
 
-export const TODOS = atom({ key: 'todos', default: [], });
+export const TODOS = atom({
+  key: 'todos', default: [
+    { id: 1, title: "GO TO GYM", isCompleted: false }
+  ],
+});
--- pages/todolist.tsx
+++ pages/todolist.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import { TODOS } from '@/RECOIL/atoms'
+import React, { useState } from 'react'
+import { useRecoilState } from 'recoil'
 
 const TodoListPage = () => {
+
+  const [Todos, setTodos] = useRecoilState(TODOS)
+  const [title, setTitle] = useState('')
+
+  function addTodoHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    let mainTodo = { id: Math.random(), title: title, isCompleted: false }
+    const copy = [...Todos]
+    copy.push(mainTodo)
+    setTodos(copy)
+    setTitle('')
+  }
+
   return (
     <main className='w-screen h-screen flex flex-col'>
 
-      <form id="input" className='w-screen flex py-2 items-center justify-center space-x-1 bg-gradient-to-r from-slate-900 via-slate-800 to-zinc-900'>
-        <input type="text" placeholder='Add Todo' className='px-2 py-1 rounded-md text-black font-bold ' />
-        <button className='bg-green-600 p-1 rounded-md'>➕</button>
+      <form id="input" onSubmit={addTodoHandler} className='w-screen flex py-2 items-center justify-center space-x-1 bg-gradient-to-r from-slate-900 via-slate-800 to-zinc-900'>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder='Add Todo' className='px-2 py-1 rounded-md text-black font-bold ' />
+        <button type='submit' className='bg-green-600 p-1 rounded-md'>➕</button>
       </form>
 
       <div id="todos">
-        <ul>
-
+        <ul className='w-fit h-fit px-6 py-10 border list-inside list-disc mx-auto mt-10 bg-slate-900/50 flex flex-col text-justify items-center justify-center text-3xl font-bold'>
+          {Todos.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
         </ul>
       </div>
     </main>
