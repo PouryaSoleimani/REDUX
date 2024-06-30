1dcd6a5bb84cabee87b9983c36df890e7226826c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 05:25:09 -0700

    reducer

---
+++ COUNTER/COUNTER_REDUCER.ts
@@ -0,0 +1,4 @@
+const incButton = document.querySelector("#incButton") as HTMLButtonElement
+const decButton = document.querySelector("#decButton") as HTMLButtonElement
+const resButton = document.querySelector("#resButton") as HTMLButtonElement
+const numElem = document.querySelector("#numElem") as HTMLHeadingElement
--- pages/counter.tsx
+++ pages/counter.tsx
@@ -7,12 +7,12 @@ const Counter = () => {
     <main className='flex items-center justify-center w-screen h-screen'>
 
       <div id="counter" className=' flex flex-col items-center py-32 justify-between bg-zinc-900 w-[30%] h-[80%] rounded-xl border-8 border-zinc-400'>
-        <h1 className='text-6xl font-extrabold '>0</h1>
+        <h1 id='numElem' className='text-6xl font-extrabold '>0</h1>
 
         <div id='Buttons' className='flex items-center justify-center space-x-2 mt-10'>
-          <button className=' w-28 px-3 py-4 rounded-lg bg-red-500 text-4xl font-extrabold'>-</button>
-          <button className=' w-28 px-3 py-4 rounded-lg bg-sky-500 text-4xl font-extrabold'>↺</button>
-          <button className=' w-28 px-3 py-4 rounded-lg bg-green-500 text-4xl font-extrabold'>+</button>
+          <button id='decButton' className=' w-28 px-3 py-4 rounded-lg bg-red-500 text-4xl font-extrabold'>-</button>
+          <button id='resButton' className=' w-28 px-3 py-4 rounded-lg bg-sky-500 text-4xl font-extrabold'>↺</button>
+          <button id='incButton' className=' w-28 px-3 py-4 rounded-lg bg-green-500 text-4xl font-extrabold'>+</button>
         </div>
 
       </div>
