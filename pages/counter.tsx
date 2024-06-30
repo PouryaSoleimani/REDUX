bf534b7c0adac2397cc5f980a09bf7c5a2509334
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 08:22:46 -0700

    todolist

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -10,14 +10,13 @@ import { COUNTERSTATE } from '@/RECOIL/atoms'
 const CounterPage = () => {
 
   const [COUNT, SETCOUNT] = useRecoilState(COUNTERSTATE)
-  console.log(COUNT)
 
   return (
     <section className='flex flex-col '>
       <div>
         <h1 className='bg-gradient-to-r from-purple-800 via-orange-600 to-yellow-400 text-3xl font-extrabold py-2 text-center'>COUNTER PAGE</h1>
       </div>
-      
+
       <div id="Container" className='bg-zinc-700 rounded-lg min-w-[30%] min-h-[70%] mt-10 mx-auto flex flex-col items-center justify-center'>
         <h1 className='text-4xl font-extrabold mt-4'>{COUNT}</h1>
 
