b26ed7df33da4785c3582b56cefa32d4424dde24
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 05:27:14 -0700

    reducer

--- COUNTER/COUNTER_REDUCER.ts
+++ COUNTER/COUNTER_REDUCER.ts
@@ -1,4 +1,18 @@
+//^ COUNTER_REDUCER
 const incButton = document.querySelector("#incButton") as HTMLButtonElement
 const decButton = document.querySelector("#decButton") as HTMLButtonElement
 const resButton = document.querySelector("#resButton") as HTMLButtonElement
 const numElem = document.querySelector("#numElem") as HTMLHeadingElement
+
+
+type ActionType2 = { type: string }
+//~ REDUCER
+function counterReducer(action: ActionType2) {
+  switch (action.type) {
+    case "INCREMENT": { }
+    case "DECREMENT": { }
+    case "RESET": { }
+    default: { }
+  }
+}
+
