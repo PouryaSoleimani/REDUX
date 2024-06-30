71c59d2d723d16ac3a77490d18e07a5098e7e47c
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 03:24:07 -0700

    STORE

--- PURE TS/REDUX_STORE.ts
+++ PURE TS/REDUX_STORE.ts
@@ -11,7 +11,11 @@ export let todolistStore = [ // Array Type Store Variable
 
 // SHOP STORE 
 let shopStore = { // Object Type Store Variable
-  prodcuts: [],
+  prodcuts: [
+    { id: 1, title: "pants", price: 32 },
+    { id: 1, title: "pants", price: 145 },
+    { id: 1, title: "pants", price: 77 },
+  ],
   users: [],
   admins: [],
   orders: [],
