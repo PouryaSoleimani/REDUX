fb9cd7d327e09ec92cc462b8d3f2e9537eae4271
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 04:14:34 -0700

    ACTIONS

--- PURE TS/REDUX_REDUCER.ts
+++ PURE TS/REDUX_REDUCER.ts
@@ -1,15 +1,20 @@
 //^ REDUX REDUCER =====================================================================================================================================================
-//COUNTER ACTIONS
+//* COUNTER ACTIONS
 let incrementAction = { type: 'INCREMENT' }
 let decrementAction = { type: 'DECREMENT' }
 
 let newStore = 0
-//COUNTER REDUCER 
+// * COUNTER REDUCER 
 function counterReducer(action: { type: string }) {
   switch (action.type) {
 
-    case "INCREMENT": { return newStore + 1 }
-    case "DECREMENT": { return newStore - 1 }
+    case "INCREMENT": {
+      return newStore + 1
+    }
+    case "DECREMENT": {
+      return newStore - 1
+    }
+
 
     default:
       break;
