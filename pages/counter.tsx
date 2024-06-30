4e9d414656110979b2b780c955de88d377def933
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 05:19:24 -0700

    reducer

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -4,7 +4,16 @@ import React from 'react'
 
 const Counter = () => {
   return (
-    <div>Counter</div>
+    <main className='flex items-center justify-center w-screen h-screen'>
+      <div id="counter" className=' flex flex-col items-center justify-center bg-zinc-900 w-[30%] h-[80%] rounded-xl border-8 border-zinc-400'>
+        <h1 className='text-6xl'>0</h1>
+        <div id='Buttons' className='flex items-center justify-center space-x-2 mt-10'>
+          <button className=' w-28 px-3 py-4 rounded-lg bg-red-500 text-4xl font-extrabold'>-</button>
+          <button className=' w-28 px-3 py-4 rounded-lg bg-sky-500 text-4xl font-extrabold'>↺</button>
+          <button className=' w-28 px-3 py-4 rounded-lg bg-green-500 text-4xl font-extrabold'>+</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
