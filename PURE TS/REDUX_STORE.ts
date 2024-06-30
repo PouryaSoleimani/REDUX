1c635bdae2f7df4a83e6b03fc77cd0342fa750b9
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-30 03:11:16 -0700

    STORE

--- PURE TS/REDUX_STORE.ts
+++ PURE TS/REDUX_STORE.ts
@@ -1,1 +1,20 @@
 //& REDUX STORE ================================================================================================================================================>
+//^ COUNTER STORE
+let counterStore = 0
+
+
+// TODO LIST STORE
+let todolistStore = [
+  { id: 1, title: "learn JS", createdAT: "1402-02-09", isCompleted: false },
+  { id: 1, title: "learn REDUX", createdAT: "1402-02-10", isCompleted: false }
+]
+
+//* SHOP STORE
+
+let shopStore = {
+  prodcuts: [],
+  users: [],
+  admins: [],
+  orders: [],
+  offerCodes: [],
+}
