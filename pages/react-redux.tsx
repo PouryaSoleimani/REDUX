e02f85e6b27ef71f9c884189999628ea51393b99
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-01 09:23:00 -0700

    REDUX

--- pages/react-redux.tsx
+++ pages/react-redux.tsx
@@ -8,23 +8,16 @@ const ReactReduxPage = () => {
   const count = useSelector((state: any) => state.counter.count)
   const dispatch = useDispatch();
 
-  //REACT USESTATE
-  const [counter, setCounter] = useState(0)
-  const increment = () => { setCounter(counter => counter + 1) }
-  const decrement = () => { setCounter(counter => counter - 1) }
-  const reset = () => { setCounter(0) }
-
-
   //RETURN
   return (
     <main>
       <h1 className='bg-gradient-to-r from-purple-950 via-purple-800 to-purple-600 py-3 text-center text-4xl font-extrabold'>React-Redux Page</h1>
       <div className='w-full text-center mt-10 text-6xl font-bold'>
-        <h1>{counter}</h1>
+        <h1>{count}</h1>
         <div className='flex items-center justify-center space-x-5 mt-8'>
-          <button onClick={increment} className='bg-green-700 px-7 h-20 text-md rounded-lg'>+</button>
-          <button onClick={reset} className='bg-sky-700 px-6 h-20 text-[30px] rounded-lg'>↻</button>
-          <button onClick={decrement} className='bg-red-700 px-8 h-20 text-md rounded-lg'>-</button>
+          <button onClick={() => dispatch(increment())} className='bg-green-700 px-7 h-20 text-md rounded-lg'>+</button>
+          <button className='bg-sky-700 px-6 h-20 text-[30px] rounded-lg'>↻</button>
+          <button onClick={() => dispatch(decrement())} className='bg-red-700 px-8 h-20 text-md rounded-lg'>-</button>
         </div>
       </div>
     </main>
