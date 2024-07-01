c1d26c127adbf4b2bd083ffcfa867b81ab4b1445
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-01 08:39:09 -0700

    REDUX

--- RECOIL/atoms.js
+++
@@ -1,9 +0,0 @@
-//^ ATOMS =========================================================================================================================================================================>
-import { atom } from "recoil";
-import { recoilPersist } from 'recoil-persist'
-const { persistAtom } = recoilPersist()
-
-export const COUNTERSTATE = atom({ key: 'counterstate', default: 0, effects_UNSTABLE: [persistAtom] });
-
-
-export const TODOS = atom({ key: 'todos', default: [], effects_UNSTABLE: [persistAtom] });
