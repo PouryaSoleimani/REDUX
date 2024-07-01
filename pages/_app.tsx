2fe442f2f3ef7c46c992de5b0553ad96c2bebda7
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-01 05:53:27 -0700

    REACT-REDUX

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+//REDUX
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
 
--- pages/react-redux.tsx
+++ pages/react-redux.tsx
@@ -1,9 +1,17 @@
 //^ REACT-REDUX PAGE
 import React, { useState } from 'react'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { counterIncrementAction, counterDecrementAction } from '@/redux/actionCreators'
+import store from '@/redux/store'
 
 const ReactReduxPage = () => {
+  //REDUX
+  const INCREMENT = useSelector(counterIncrementAction)
+  const DECREMENT = useSelector(counterDecrementAction)
+  const dispatch = useDispatch()
 
+
+  //REACT USESTATE
   const [counter, setCounter] = useState(0)
   const increment = () => { setCounter(counter => counter + 1) }
   const decrement = () => { setCounter(counter => counter - 1) }
@@ -17,9 +25,9 @@ const ReactReduxPage = () => {
       <div className='w-full text-center mt-10 text-6xl font-bold'>
         <h1>{counter}</h1>
         <div className='flex items-center justify-center space-x-5 mt-8'>
-          <button className='bg-green-700 px-7 h-20 text-md rounded-lg' onClick={increment}>+</button>
-          <button className='bg-sky-700 px-6 h-20 text-[30px] rounded-lg' onClick={reset}>↻</button>
-          <button className='bg-red-700 px-8 h-20 text-md rounded-lg' onClick={decrement}>-</button>
+          <button className='bg-green-700 px-7 h-20 text-md rounded-lg'>+</button>
+          <button className='bg-sky-700 px-6 h-20 text-[30px] rounded-lg'>↻</button>
+          <button className='bg-red-700 px-8 h-20 text-md rounded-lg'>-</button>
         </div>
       </div>
     </main>
