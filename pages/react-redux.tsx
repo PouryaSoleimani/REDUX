f13651444709aabd27b94d3bacf9de69e3a8d03d
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-01 02:54:51 -0700

    'react-redux

---
+++ pages/react-redux.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ReactReduxPage = () => {
+  return (
+    <main>
+      <h1 className='bg-gradient-to-r from-purple-950 via-purple-800 to-purple-600 py-3 text-center text-4xl font-extrabold'>React-Redux Page</h1>
+      
+    </main>
+  )
+}
+
+export default ReactReduxPage
