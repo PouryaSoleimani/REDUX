7f387345afbf32152c38c43003830e7cba69de84
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-01 05:20:16 -0700

    REACT-REDUX

--- redux/actionCreators.js
+++ redux/actionCreators.js
@@ -2,3 +2,10 @@
 
 import { counterIncrement, counterDecrement } from "./actionTypes";
 
+export const counterIncrementAction = () => {
+  return { type: counterIncrement }
+}
+
+export const counterDecrementAction = () => {
+  return { type: counterDecrement }
+}
