a89971d20cf069b7a3bd4255ac688fa52b0de36d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-21 13:19:42 +0330

    🟪 REDUX 🟪

--- pages/test.js
+++ pages/test.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const test = () => {
   // PURE FUNCTION => HAVE SAME OUTPUT EVERYTIME
-  function DoubleNumber(number) {
+  function DoubleNumberPure(number) {
+    return number * 2;
+  }
+
+  // IMPURE FUNCTION => HAVE SIDE EFFECTS (console.log)
+  function DoubleNumber2(number) {
+    console.log("LOG LOG");
     return number * 2;
   }
 
@@ -11,7 +17,8 @@ const test = () => {
     return number * Math.random();
   }
 
-  console.info("PURE FUNCTION ==>", DoubleNumber(10));
+  console.info("PURE FUNCTION ==>", DoubleNumberPure(10));
+  console.info("IMPURE FUNCTION ==>", DoubleNumber2(10));
   console.info("IMPURE FUNCTION ==>", randomNumber(10));
 
   return <div>test</div>;
