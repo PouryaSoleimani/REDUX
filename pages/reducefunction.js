e0918c82e14be495620e83ed79afe007be263db1
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-21 14:09:36 +0330

    🟪 REDUX 🟪

--- pages/reducefunction.js
+++ pages/reducefunction.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const reducerfunction = () => {
+  //  FIRST TEST
   const numArrays = [1, 2, 3, 4, 5];
 
   const sumNumber = numArrays.reduce((a, b) => {
@@ -8,6 +9,17 @@ const reducerfunction = () => {
   });
 
   console.info("SUM ====> ", sumNumber);
+  // SECOND TEST
+  const numArrays2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  const sum2 = numArrays2.reduce((firstArray, secondArray) => {
+    return [...firstArray, ...secondArray];
+  });
+
+  console.info("SUM 2 =========>", sum2);
 
   return (
     <div>
