e583d11f52aef97c454e8c64543fafd53e9195dd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-21 14:25:04 +0330

    🟪 REDUX 🟪

--- pages/hof.js
+++ pages/hof.js
@@ -1,43 +1,14 @@
 import React from "react";
 
 const hof = () => {
+    // NORMAL FUNCTION
+  function randomNumebr() {
+    return Math.floor(Math.random() * 1000);
+  }
+  console.info(randomNumebr());
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
   return (
     <div>
       <h1 className="p-4 text-center font-black text-3xl text-black bg-[#FABB14]">
