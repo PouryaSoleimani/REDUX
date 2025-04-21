b504739c7773c265d4c24ce33caa5ca31d87b627
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-21 13:42:17 +0330

    🟪 REDUX 🟪

---
+++ pages/reducefunction.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const reducerfunction = () => {
+
+
+
+
+
+
+
+
+
+
+
+  
+  return (
+    <div>
+      <h1 className="p-4 text-center font-black text-3xl text-black bg-[#FABB14]">
+        REDUCER FUNCTIONS
+      </h1>
+    </div>
+  );
+};
+
+export default reducerfunction;
