5aa043309678ffe03fa4b7e4a8f2882db578f7db
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-21 14:22:02 +0330

    🟪 REDUX 🟪

---
+++ pages/hof.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const hof = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  return (
+    <div>
+      <h1 className="p-4 text-center font-black text-3xl text-black bg-[#FABB14]">
+        HOF FUNCTIONS
+      </h1>
+    </div>
+  );
+};
+
+export default hof;
