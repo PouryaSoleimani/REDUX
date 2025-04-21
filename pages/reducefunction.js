b31b850054ff8d5169516d979025a60bec7ebc06
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-21 13:47:09 +0330

    🟪 REDUX 🟪

--- pages/reducefunction.js
+++ pages/reducefunction.js
@@ -1,18 +1,12 @@
 import React from "react";
 
 const reducerfunction = () => {
+  const numArrays = [10, 20, 30, 40, 50];
+  const sumNumber = numArrays.reduce((a, b) => {
+    console.log(a, b);
+    return a + b;
+  });
 
-
-
-
-
-
-
-
-
-
-
-  
   return (
     <div>
       <h1 className="p-4 text-center font-black text-3xl text-black bg-[#FABB14]">
