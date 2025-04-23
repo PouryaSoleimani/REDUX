1bbc1fb8de813a192564cebe430b9108798411f6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-23 12:26:05 +0330

    REDUX

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,16 @@
 //* REDUX STORE FILE
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "@/features/counter/counterSlice"
+import counterReducer from "@/features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-  }
-})
+  },
+});
+
+export const counterStore = 0;
+
+export const todolistStore = [
+  { id: 1, title: "learn js", createdAT: "today", isDone: false },
+  { id: 2, title: "learn redux", createdAT: "yesterday", isDone: false },
+];
