39afab5e2a0c30bf61891acb45bc33a7e931d59c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-23 10:44:25 +0330

    REDUX

---
+++ pages/mutation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mutation = () => {
+    return (
+        <div>
+            <h1 className="p-4 text-center font-black text-3xl text-black bg-[#FABB14]">  MUTATIONS  </h1>
+        </div>
+    )
+}
+
+export default mutation
