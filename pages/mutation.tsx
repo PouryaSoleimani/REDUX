def499ec44ee042718b908fab0519626ee1ffbfe
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-23 10:53:45 +0330

    REDUX

--- pages/mutation.tsx
+++ pages/mutation.tsx
@@ -1,6 +1,42 @@
 import React from 'react'
 
 const mutation = () => {
+    let number1 = "12";
+    let number2 = number1;
+    number2 = "10"
+    console.info(number1, number2)
+
+    let obj1 = {
+        id: 1,
+        title: "Fake Title"
+    };
+
+    let obj2 = obj1;
+    console.info(obj1, obj2)
+
+    obj2.title = "REAL TITLE"
+    console.info(obj1, obj2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div>
             <h1 className="p-4 text-center font-black text-3xl text-black bg-[#FABB14]">  MUTATIONS  </h1>
