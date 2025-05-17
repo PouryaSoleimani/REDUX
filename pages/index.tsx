c833229f0b40b6bf2b2c9f7924f09a017143656c
Pourya_Delivando <Pourya@delivando.com>
2025-05-17 20:09:52 +0330

    REFACTOR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
-//^ REDUX HOME PAGE ___________________________________________________________________________________________________________________________________________________________________ 
-import React from 'react'
+//^ REDUX HOME PAGE ___________________________________________________________________________________________________________________________________________________________________
+import React from "react";
 
 const Home = () => {
+  const fruitsArray = [
+    ["apple", "banana"],
+    ["cherry", "date"],
+    ["elderberry", "fig"],
+  ];
+  const reduceddFruits = fruitsArray.reduce((a, b) => {
+    return a.concat(b);
+  });
+
+  console.info("FRUITS =====>", reduceddFruits);
+
   return (
     <>
       <div>
-        <h1 className='text-3xl font-extrabold bg-purple-700 text-center py-2 border-b-8'>REDUX HOME PAGE</h1>
+        <h1 className="text-3xl font-extrabold bg-purple-700 text-center py-2 border-b-8">
+          REDUX HOME PAGE
+        </h1>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
